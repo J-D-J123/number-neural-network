f3aa458d100032aab6fc8e951a729438f15d4ef4
J-D-J123 <jdjohnson1506@gmail.com>
2026-07-15 21:59:37 -0500

    Hi inital commit, Joey was here :)

---
+++ src/Layers.c
@@ -0,0 +1,6 @@
+/**
+ * Author:      Joseph Johnson 
+ * file:        layers.c
+ * Desc:        setup the nerual network via layers 
+ *              which is set in stone in settings.h 
+ */
---
+++ src/main.c
@@ -0,0 +1,24 @@
+/**
+ * Author:          Joseph Johnson 
+ * Date:            06/09/2026
+ * File:            starts the neural network and sets it up from the first layer
+ *                      then sends data through the network to determine what image it is
+ */
+
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+// include all custom header files
+#include "../structs/read_png.h"
+
+
+// first get each pixel and put it into a row & colummn and take each pixel and put into each neruon 
+int main() {
+
+    // read the png image first 28 * 28 pixels
+
+    
+
+    return 0;     
+}
