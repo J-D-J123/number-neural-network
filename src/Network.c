815b2553eeac0adacbff0d9b29864e4c972aa80e
J-D-J123 <jdjohnson1506@gmail.com>
2026-07-18 23:25:08 -0500

    initalized sigmoid function & ReLU functions for training the FFNN (feed-forward neural network logic)
    - deleted Neuron.c - was unused.
    
    - Changed weights from a value from [-1.0, 1.0] to random values inbetween [-1.0, 1.0] inclusive
    
    - Added sources used, used limited OpenAI usage for questions about differences in each neural network training algorithm Ex. ReLU vs sigmoid func.

--- src/Network.c
+++ src/Network.c
@@ -7,14 +7,15 @@
 
 #include "../structs/network.h"
 
-
+// example for per layer neuron count
 // [784] [256] [128] [10] 
 /**
  * Function setup_network(int * neurons_per_layer, int num_of_layers) 
  * sets up a neural network based on it's prams 
+ * 
  * @param neurons_per_layer is an integer array that represents numbers of the total number of neurons per layer 
  * @param num_of_layers is an integer that represents the total number of layers
- * @returns a Network struct 
+ * @returns a Network struct fully setup from input -> hidden -> output via connections
  */
 Network setup_network(int * neurons_per_layer, int num_of_layers) {
 
@@ -69,8 +70,10 @@ Network setup_network(int * neurons_per_layer, int num_of_layers) {
                 // middle layer neuron connects to input layer neuron
                 network.connections[connection_index].next = &current->neurons[k]; 
 
-                // initialize the weights of each Connection
-                network.connections[connection_index].weight = 0.0f; 
+                // initialize the weights randomly and not just 0.0f of each Connection
+                // network.connections[connection_index].weight = 0.0f; 
+                // generate weight with value from [-1.0, 1.0]
+                network.connections[connection_index].weight = ((float) rand() / RAND_MAX) * 2.0f - 1.0f; 
 
                 connection_index++; 
             }
@@ -79,3 +82,120 @@ Network setup_network(int * neurons_per_layer, int num_of_layers) {
 
     return network; 
 }
+
+/**
+ * this forward_backpropagation_algorithm(...) is the method for the network to learn 
+ * 
+ * @param network is the set up connected network from the function setup_network(...)
+ * @param input_pngs is an array of input images used to train the network for multiple images 
+ * @returns network which is the modified network after the new "training"
+ */
+Network train_network(Network network, png * input_pngs, int num_of_pngs) { 
+
+    // Learning alorithm... TODO; 
+    /**
+     *  Forward pass, compute activations layer by layer by adjusting the biases
+     * 
+     *  Loss compare the 10 outputs aka what number it is to the true number value, look into
+     *          softmax on the output layer + cross-entropy loss (whatever that is)
+     * 
+     *  Backpropagation compute how much each weight/bias contributed to the error aka the gradient
+     * 
+     *  Gradient descent - stochastic gradient descent (SGD) in mini batches
+     *  Adam is a fancier optimizer i can add later. nudge every weight/bias in the direction that reduces loss. 
+     */
+
+}
+
+void forward_pass(Network * network, png * image) {
+
+    // each neuron (z)
+    // z = SUM (inputs x weights) + bias
+    // then use an activation function f(z)
+
+    // activation is just a sum of all the neurons to the next layer aka 
+    // if the sum = 0.56
+    // sigmoid(0.56) = 0.636
+
+
+
+}
+
+void backpropagation(Network * network, int label) {
+
+
+}
+
+void update_parameters(Network * network, double learning_rate) {
+
+
+}
+
+double compute_loss(Network * network, int label) {
+
+
+}
+
+
+/**
+ * compute_ReLU(double num) applies the ReLU activation function 
+ * https://en.wikipedia.org/wiki/Rectified_linear_unit
+ * 
+ * ReLU(x) = max(0, x) 
+ * 
+ * @param num is the x variable value input into the equasion 
+ */
+double compute_ReLU(double num) {
+
+    if (num > 0) {
+
+        return num; 
+    }
+
+    return 0.0; 
+}
+
+/**
+ * compute_ReLU(double num) applies the ReLU activation function for backpropagation 
+ * https://en.wikipedia.org/wiki/Rectified_linear_unit
+ * 
+ * ReLU(x) = max(0, x) {1, x > 0} {0, x <= 0}
+ * 
+ * @param num is the x variable value input into the equasion 
+ */
+double compute_ReLU_derivative(double num) {
+
+    if (num > 0) {
+
+        return 1.0; 
+    }
+
+    return 0.0; 
+}
+
+
+/**
+ * compute_softplus(double num) applies the softplus activaton function 
+ * Softplus is a smooth approximation of ReLU 
+ * 
+ * softplus(x) = log(1 + exp(x))
+ * 
+ * @param num is the value estimated 
+ * @returns a double value of estimated value
+ */
+double compute_softplus(double num) {
+
+    return log(1 + exp(num)); 
+}
+
+// /**
+//  * // commeted out because i guess it is not useful and sort of outdated :( used ReLu instead
+//  * compute_sigmoid(double num) returns the value of the func 
+//  *
+//  * @param num 
+//  * @returns double
+//  */
+// double compute_sigmoid(double num) {
+
+//     return (1.0 / (1.0 + exp(-num)));
+// }
--- structs/network.h
+++ structs/network.h
@@ -8,6 +8,13 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include <stdio.h> 
+#include <stdlib.h>
+#include <math.h>  
+#include <time.h> 
+
+#include "../structs/png.h"
+
 typedef struct Neuron {
 
     float activation; 
@@ -41,5 +48,17 @@ typedef struct Network {
 
 // --------- FUNCTIONS ----------
 Network setup_network(int * neurons_per_layer, int num_of_layers); 
+
+// training the network...
+Network train_network(Network network, png * input_pngs, int num_of_pngs);
+void forward_pass(Network * network, png * image);
+void backpropagation(Network * network, int label);
+void update_parameters(Network * network, double learning_rate); 
+double compute_loss(Network * network, int label);
+
+// computing functions 
+double compute_ReLU(double num); 
+double compute_ReLU_derivative(double num);
+double compute_softplus(double num); 
 
 #endif
